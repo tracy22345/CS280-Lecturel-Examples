85bbf0d1a03932a9dcac7de4874ca1983bd9ad16
FrankLikuohao <frank.likuohao@gmail.com>
2017-03-03 22:23:17 +0800

    寶可夢

---
+++ CS280-Lecturel-Examples/CS280-Lecturel-Examples/Pokemon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS280_Lecturel_Examples
+{
+    internal class Pokemon
+    {
+        public float Height;
+        public float Weight;
+        public string Abilities;
+        public string Gategory;
+        public string Gender;
+    }
+}
--- CS280-Lecturel-Examples/CS280-Lecturel-Examples/Program.cs
+++ CS280-Lecturel-Examples/CS280-Lecturel-Examples/Program.cs
@@ -10,7 +10,14 @@ namespace CS280_Lecturel_Examples
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("ya i see");
+            Pokemon pokemon = new Pokemon();
+            pokemon.Height = 2.0f;
+            pokemon.Weight = 12.7f;
+            pokemon.Gategory = "Lizard";
+            pokemon.Abilities = "Blaze";
+            pokemon.Gender = "M";
+            Console.WriteLine("身高:{0}公尺,體重{1}磅",
+            pokemon.Height, pokemon.Weight);
         }
     }
 }
